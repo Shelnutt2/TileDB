17d2917cf6aa583cf436aed754cc3f62bf3dd09c
Tyler Denniston <tyler@tiledb.io>
2019-03-20 13:53:52 -0400

    Encapsulate generated Capnp types in namespace tiledb::rest::capnp.
    Part of #3.

--- tiledb/sm/c_api/tiledb.cc
+++ tiledb/sm/c_api/tiledb.cc
@@ -2236,7 +2236,7 @@ int tiledb_array_schema_serialize(
   if (sanity_check(ctx, array_schema) == TILEDB_ERR)
     return TILEDB_ERR;
 
-  auto st = tiledb::rest::array_schema_serialize(
+  auto st = tiledb::rest::capnp::array_schema_serialize(
       array_schema->array_schema_,
       (tiledb::sm::SerializationType)serialize_type,
       serialized_string,
@@ -2268,7 +2268,7 @@ int tiledb_array_schema_deserialize(
     save_error(ctx, st);
     return TILEDB_OOM;
   }
-  auto st = tiledb::rest::array_schema_deserialize(
+  auto st = tiledb::rest::capnp::array_schema_deserialize(
       &((*array_schema)->array_schema_),
       (tiledb::sm::SerializationType)serialize_type,
       serialized_string,
@@ -2624,7 +2624,7 @@ int tiledb_query_serialize(
   if (sanity_check(ctx, query) == TILEDB_ERR)
     return TILEDB_ERR;
 
-  tiledb::sm::Status st = tiledb::rest::query_serialize(
+  tiledb::sm::Status st = tiledb::rest::capnp::query_serialize(
       query->query_,
       (tiledb::sm::SerializationType)serialize_type,
       serialized_string,
@@ -2651,7 +2651,7 @@ int tiledb_query_deserialize(
   if (sanity_check(ctx, query) == TILEDB_ERR)
     return TILEDB_ERR;
 
-  tiledb::sm::Status st = tiledb::rest::query_deserialize(
+  tiledb::sm::Status st = tiledb::rest::capnp::query_deserialize(
       query->query_,
       (tiledb::sm::SerializationType)serialize_type,
       serialized_string,
